4202fd3e19d4b6e8d59b1d9c1766563db923dfd7
JoJosefe <147321018+Seifex01@users.noreply.github.com>
2024-02-21 09:12:13 +0100

    Actualización de espaciado entre cards.

--- app/src/main/java/com/example/superheroes/MainActivity.kt
+++ app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -75,7 +76,10 @@ fun SuperHeroApp(modifier: Modifier = Modifier) {
 
                 LazyColumn(contentPadding = it) {
                     items(heroes) {
-                        HeroCard(hero = it)
+                        Spacer(modifier = Modifier.height(8.dp))
+                        HeroCard(hero = it,
+                            modifier = Modifier
+                                .padding(horizontal = 16.dp))
                     }
                 }
 
